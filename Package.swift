1ebb04f7ad38c39c43d7e575190726d75c7159b6
Nate Armstrong <natearmstrong2@gmail.com>
2020-10-01 10:22:28 -0600

    Set type to .dynamic

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "PSPDFKit",
+            type: .dynamic,
             targets: ["PSPDFKit", "PSPDFKitUI"]),
     ],
     targets: [
